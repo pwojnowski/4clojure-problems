85b90a33e78aed00e907967cf93e75b440d14ca5
Przemysław Wojnowski <esperanto@cumego.com>
2015-03-03 20:44:51 +0100

    Added criterium for benchmarking.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject four-clojure "0.1.0-SNAPSHOT"
   :description "4clojure.com solutions"
   :url "http://4clojure.com"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [criterium "0.4.3"]])
